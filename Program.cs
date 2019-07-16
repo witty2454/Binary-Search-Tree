b6a27a57f4e3695fa02e06537d9f9944dd49c6c8
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-16 10:24:36 -0500

    added the binarySearchTree method to my program class.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+
+            tree.AddNode(10);
+            tree.AddNode(8);
+            tree.AddNode(3);
+            tree.AddNode(7);
+            tree.AddNode(1);
+            tree.AddNode(4);
+            tree.AddNode(5);
+            tree.AddNode(2);
+            tree.AddNode(9);
+            tree.AddNode(6);
+
+            tree.Display();
+            Console.ReadLine();
+        }
+    }
+}
