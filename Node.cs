6e87a05f5e24d317a84d019443ce09f8f32cad5d
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-16 10:48:46 -0500

    added to node class
    Added the variables, and methods i will need in this class to make it work.

---
+++ Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int number;
+        public Node rightBranch;
+        public Node leftBranch;
+
+        public Node(int number)
+        {
+            this.number = number;
+            rightBranch = default(Node);
+            leftBranch = default(Node);
+        }
+        public void InsertNode(ref Node root, int input)
+        {
+            if (root == null)
+            {
+                root = new Node(input);
+            }
+            else if (root.number > input)
+            {
+                InsertNode(ref root.leftBranch, input);
+            }
+            else if (root.number < input)
+            {
+                InsertNode(ref root.rightBranch, input);
+            }
+        }
+
+        public void TakeInNumber(Node node)
+        {
+            if (node == null)
+                return;
+            TakeInNumber(node.leftBranch);
+            TakeInNumber(node.rightBranch);
+            Console.WriteLine(" " + node.number);
+
+        }
+    }
+}
