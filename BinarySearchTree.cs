d35e70bb9592dcbb19c5504fcef754301ba73904
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-16 13:10:12 -0500

    Resubmitting
    Resubmitting my add and search methods, because i added wrong file. Ooops.

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinarySearchTree2
+namespace BinarySearchTree
 {
     class BinarySearchTree
     {
+        Node root;
+        int count = 0;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void AddNode(int number)
+        {
+            if (root == null)
+            {
+                root = new Node(number);
+            }
+            else
+            {
+                root.InsertNode(ref root, number);
+            }
+            count++;
+        }
+        public void Display()
+        {
+            root.TakeInNumber(root);
+        }
+        public bool search(Node node, int data)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.number == data)
+            {
+                return true;
+            }
+            else if (node.number < data)
+            {
+                return search(node.leftBranch, data);
+            }
+            else if (node.number > data)
+            {
+                return search(node.rightBranch, data);
+            }
+            return false;
+        }
     }
 }
